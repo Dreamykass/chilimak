1845acd68621850808585a490b1cc1c92b9cb134
Damian Grüner <grunerdamian@gmail.com>
2021-06-06 14:31:32 +0200

    workin

--- common/src/parser/mod.rs
+++ common/src/parser/mod.rs
@@ -3,8 +3,14 @@ use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub grammar);
 
+// #[allow(clippy::style, clippy::complexity, clippy::perf)]
+// mod grammar {
+//     include!(concat!(env!("OUT_DIR"), "/grammar.rs"));
+// }
+
 pub fn parse(tokens: Vec<Token>) {
-    let ast =
-        grammar::DefinitionListParser::new().parse(tokens.into_iter().map(|t| (0usize, t, 0usize)));
+    let tokens: Vec<(usize, Token, usize)> =
+        tokens.into_iter().map(|t| (0usize, t, 0usize)).collect();
+    let ast = grammar::DefinitionListParser::new().parse(tokens);
     println!("{:?}", ast);
 }
